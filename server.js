d18abdda283553e0440dbd30831f08e040568dda
Roberto Cunha <robertocunha@gmail.com>
2025-08-29 00:14:50 -0300

    Feat: Autenticacaovia header

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 import express from 'express';
 const app = express();
-const PORT = 3001;
-
 app.use(express.json());
+
+const PORT = 3001;
+const HEADER_SECRET = '767670Fo!';
 
 app.post('/teleconsulta', (req, res) => {
     const payload = req.body;
     console.log(('[teleconsulta] payload: ', payload));
-    res.status(200).json({ok: true});
+
+    if (req.headers['x-secret'] !== HEADER_SECRET) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    res.status(200).json({ ok: true });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em localhost:${PORT}`);
+    console.log(`Servidor rodando em localhost:${PORT}`);
 });
